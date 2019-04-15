90906eb16b22e12cf03581abc115671438b5c609
e <819589912@qq.com>
2019-04-15 19:25:33 +0800

    初建前台项目

---
+++ reception-eureka/src/main/java/com/jk/ReceptionEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.jk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceptionEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReceptionEurekaApplication.class, args);
+    }
+
+}
