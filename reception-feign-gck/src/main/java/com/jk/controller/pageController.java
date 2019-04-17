ecfaa7da7f324d6e908d9ec55567280946cb1c40
guochenkai <chenkaiwinner@163.com>
2019-04-17 09:42:19 +0800

    gck 前台咨询页面提交

---
+++ reception-feign-gck/src/main/java/com/jk/controller/pageController.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright (C), 2015-2019, XXX有限公司
+ * FileName: pageController
+ * Author:   郭晨凯
+ * Date:     2019-04-16 21:46
+ * Description: 页面跳转
+ * History:
+ * 郭晨凯        <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.jk.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈页面跳转〉
+ *
+ * @author chenkai
+ * @create 2019-04-16
+ * @since 1.0.0
+ */
+
+@Controller
+public class pageController {
+
+    @GetMapping(value = "/zixun")
+    public String zixun() {
+        return "zixun";
+    }
+}
---
+++ reception-provider-gck/src/main/java/com/jk/ReceptionProviderGckApplication.java
@@ -0,0 +1,19 @@
+package com.jk;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication(scanBasePackages = {"com.jk"})
+@EnableEurekaClient
+@MapperScan("com.jk.mapper")
+@RestController
+public class ReceptionProviderGckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReceptionProviderGckApplication.class, args);
+    }
+
+}
